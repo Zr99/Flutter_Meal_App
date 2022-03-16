4c91d195aac326171d950f6e1ff5de30108a3672
Ryan Ling <Ryan.Ling@metalform.co.nz>
2022-03-16 13:37:39 +1300

    Added the font of the titles

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -10,12 +10,18 @@ class CategoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(15),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
       decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [color.withOpacity(0.7), color],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,), borderRadius: BorderRadius.circular(15), ),
-      child: Text(title),
+        gradient: LinearGradient(
+          colors: [color.withOpacity(0.7), color],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,16 @@ class MyApp extends StatelessWidget {
         accentColor: Colors.amber,
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              titleLarge:
+                  TextStyle(fontSize: 24, fontFamily: 'RobotoCondensed'),
+            ),
       ),
       home: CategoriesScreen(),
     );
